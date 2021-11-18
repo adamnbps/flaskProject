154a81011ced2b3013331c55b8b269f0233ec6f3
adamhorvitz <horvitzadamh@gmail.com>
2021-11-18 09:36:37 -0500

    Added About Me page

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 app.secret_key = 'the random string'
